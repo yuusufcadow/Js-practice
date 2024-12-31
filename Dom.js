3ab514760898683565c9b834d4fd12c6be62223d
yuusufcadow <yuusufcadow938@gmail.com>
2025-01-01 03:26:01 +0900

    Waa ON iyo OFF oo lagu sameeyey HTML And Js

---
+++ Dom.js
@@ -0,0 +1,89 @@
+//  let elementh1 =document.getElementById("title")
+//  let btn= document.getElementsByTagName("button")[0]
+
+// let para = document.getElementsByClassName("section")[0]
+
+// let heading5 =document.querySelector("h5")
+
+//  console.log(elementh1)  
+//  console.log(btn)  
+//  console.log(para)  
+// console.log(heading5)
+
+// //  let heading=document.querySelector("#hero")
+// // let yuusuf=document.querySelector("#ero")
+// //  heading.innerHTML= "Waxaan ahay yuusuf adow mohamoud mohamed hassan macalin nour OSman sheikh" 
+// // heading.style.border= "solid red 10px"
+// //  heading.style.border="10px"  
+// //  heading.style.fontSize="50px"
+// //  heading.style.margin="100px"
+
+
+
+// // yuusuf.innerHTML= "Waxaan ahay Software devoloper"
+// //  yuusuf.style.color="white"
+// //  yuusuf.style.backgroundColor="Blue"
+
+//                                        // Assigment Making border With js
+
+// let tyle= document.querySelector("#Yuu")
+// tyle.style.width="200px"
+// tyle.style.height="200px"
+// tyle.style.border="3px solid aqua"
+// tyle.style.borderRadius="10px"
+
+
+
+// let lete= document.querySelector("#Yuuu")
+// lete.style.width="200px"
+// lete.style.height="200px"
+// lete.style.border="3px solid blue"
+// lete.style.borderRadius="10px"
+
+// let letee= document.querySelector("#uuy")
+// letee.style.width="200px"  
+// letee.style.height="200px"
+// letee.style.border="3px solid red"
+// letee.style.borderRadius="10px"
+
+// let cadow= document.querySelector("#adow")
+// cadow.style.display="flex"
+// cadow.style.gap="20px"
+// cadow.style.justifyContent="Center"
+// cadow.style.marginTop="180px"
+
+// // Exaple of 
+// let buttom = document.querySelector(".btn")
+
+// buttom.addEventListener("click",function(){
+//     document.body.style.backgroundColor = ("green")
+// })
+
+
+
+ let box=document.querySelector("#light")
+ let button1=document.querySelector("#btnON")
+ let button2=document.querySelector("#btnOff")
+
+
+
+ button1.addEventListener("click",function(){
+    box.style.backgroundColor= "Yellow"
+ })
+ 
+
+
+  button2.addEventListener("click",function(){
+    box.style.backgroundColor= "transparent"
+  })
+
+
+
+
+
+
+
+
+
+
+
